a99f8af11c95b9b0da876eeb070c5e2a5f5fb07c
Jesse Mazzie <jessegalt@yahoo.com>
2018-03-22 22:57:23 -0400

    Initial commit.

---
+++ Talker.java
@@ -0,0 +1,15 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Talker {
+    public static void main(String[] args) {
+        System.out.println("heelo!");
+    }
+    Talker(Socket s) {
+
+    }
+
+    Talker(ServerSocket s) {
+
+    }
+}
