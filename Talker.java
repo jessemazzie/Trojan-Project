6640876f551de33ea686964850b37a7575e9ac17
Jesse Mazzie <jessegalt@yahoo.com>
2018-03-25 17:18:33 -0400

    Added some data fields for talker.

--- Talker.java
+++ Talker.java
@@ -1,15 +1,33 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Talker {
-    public static void main(String[] args) {
-        System.out.println("heelo!");
+    private String ID;
+    private BufferedReader reader;
+    private DataOutputStream dos;
+
+    Talker(String domain, int portNum, String ID) throws IOException {
+        Socket s;
+
+        s = new Socket(domain, portNum);
+        //TODO: Assign reader
+        //TODO: Assign dos
+        this.ID = ID;
     }
+
     Talker(Socket s) {
-
+        //TODO: Assign reader
+        //TODO: Assign dos
     }
 
-    Talker(ServerSocket s) {
-
+    /**
+     * Simple setter for ID.
+     * @param ID
+     */
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
