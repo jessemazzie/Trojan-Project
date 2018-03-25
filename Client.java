8e005138da3991fe03c57cc1c6c019c23f81d899
Jesse Mazzie <jessegalt@yahoo.com>
2018-03-25 19:03:07 -0400

    Client JFrame displaying.

--- Client.java
+++ Client.java
@@ -1,2 +1,28 @@
-public class Client {
+import javax.swing.*;
+import java.awt.*;
+
+public class Client extends JFrame {
+    Client() {
+        Container cp;
+        cp = getContentPane();
+        
+
+        setupMainFrame();
+    }
+
+    void setupMainFrame() {
+        Toolkit tk;
+        Dimension d;
+
+        tk = Toolkit.getDefaultToolkit();
+        d = tk.getScreenSize();
+        setSize(d.width/4, d.height/4);
+        setLocation(d.width/4, d.height/4);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setTitle("Fun game that is not a trojan.");
+
+        setVisible(true);
+    }
 }
